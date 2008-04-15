027867e3f1f65fc25371eb079810d3363f65e023
Steve Sloan <steve@finagle.org>
2008-04-15 15:57:35 -0700

    Removed Rakefile, moved auto-build logic to cruise_config.rb.

--- cruise_config.rb
+++ cruise_config.rb
@@ -1,8 +1,15 @@
 Project.configure do |project|
   project.email_notifier.emails = %w/steve@finagle.org/
+  project.build_command = %q{
+    RPMDIR='/var/www/html/rpms' ;
+    make pkgs-install RPMDIR="$RPMDIR" &&
+    repomanage --keep 1 --old --space $RPMDIR | xargs rm -f  &&
+    rpm --resign $RPMDIR/libFinagle*  &&
+    createrepo $RPMDIR  &&
+    sudo rpm -Uvh $RPMDIR/libFinagle*
+  }
 
 #  project.rake_task = ''
-#  project.build_command = ''
 #  project.email_notifier.from = ''
 #  project.scheduler.polling_interval = 5.minutes
 end
