94fb45b129197a4fe95d32d6dfc0acb89c6d2d69
Steve Sloan <steve@home.finagle.org>
2008-04-15 14:48:49 -0700

    Added Rakefile, CruiseControl.rb configuration.

---
+++ cruise_config.rb
@@ -0,0 +1,9 @@
+Project.configure do |project|
+  project.email_notifier.emails = [steve@finagle.org]
+
+#  project.rake_task = ''
+#  project.build_command = ''
+#  project.email_notifier.from = ''
+#  project.scheduler.polling_interval = 5.minutes
+end
+
