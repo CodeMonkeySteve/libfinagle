e5dbaacfb2c3317f4ba79ca5e91b0f4f45a7d2cd
Steve Sloan <steve@home.finagle.org>
2008-04-15 17:14:31 -0700

    Reverted to Rakefile.  Removed 'install' from cruise task.

--- cruise_config.rb
+++ cruise_config.rb
@@ -1,13 +1,13 @@
 Project.configure do |project|
   project.email_notifier.emails = %w/steve@finagle.org/
-  project.build_command = %q{
-    RPMDIR='/var/www/html/rpms' ;
-    make pkgs-install RPMDIR="$RPMDIR" &&
-    repomanage --keep 1 --old --space $RPMDIR | xargs rm -f  &&
-    rpm --resign $RPMDIR/libFinagle*  &&
-    createrepo $RPMDIR  &&
-    sudo rpm -Uvh $RPMDIR/libFinagle*
-  }
+#  project.build_command = %q{
+#    RPMDIR='/var/www/html/rpms' ;
+#    make pkgs-install RPMDIR="$RPMDIR" &&
+#    repomanage --keep 1 --old --space $RPMDIR | xargs rm -f  &&
+#    rpm --resign $RPMDIR/libFinagle*  &&
+#    createrepo $RPMDIR  &&
+#    sudo rpm -Uvh $RPMDIR/libFinagle*
+#  }
 
 #  project.rake_task = ''
 #  project.email_notifier.from = ''
